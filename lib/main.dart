652d8bdfe55619f9d8e32b6ac2126d80012c5043
An7orAhmed <antor.mee@gmail.com>
2023-07-10 15:11:48 +0600

    path str

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,14 @@ import 'package:charger_station_web/login.dart';
 import 'package:charger_station_web/state.dart';
 import 'package:flutter/material.dart';
 import 'package:localstorage/localstorage.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 // flutter build web --release --base-href "/"
 
 void main() {
   final LocalStorage storage = LocalStorage('data');
   bool isLoggedIn = storage.getItem("login") ?? false;
+  setPathUrlStrategy();
   runApp(MaterialApp(
     title: "Charger Station",
     debugShowCheckedModeBanner: false,
